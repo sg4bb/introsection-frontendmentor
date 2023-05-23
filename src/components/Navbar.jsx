019cfc7e5b2956f76274344a5d9e443ead2551a7
Gabriel <gaabrielserraa@gmail.com>
2023-05-23 15:19:06 -0400

    done the section responsive to the navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,63 @@
-import MenuIcon from "./icons/MenuIcon";
+"use client";
+
+import { useState } from "react";
+import MenuIcon from "@/components/icons/MenuIcon";
+import CloseIcon from "@/components/icons/CloseIcon";
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const handlingMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
   return (
     <>
-      <div className="flex items-center py-6">
+      <div className="container mx-auto px-3 md:px-auto flex items-center py-6">
         {/* Brand section 🐱‍👤 */}
-        <section className="mr-20 flex-grow md:flex-none">
-          <h1 className="text-2xl font-semibold">snap</h1>
+        <section className="w-full md:w-auto md:grow-0 md:mr-20">
+          <h1 className="text-2xl font-bold">snap</h1>
         </section>
 
-        <button className="block md:hidden">
+        <button className="block flex-grow md:hidden" onClick={handlingMenu}>
           <MenuIcon />
         </button>
 
-        <nav className="flex items-center w-full">
-          {/* Navbar navigation */}
-          <section className="flex-grow hidden md:block">
-            <ul className="flex flex-wrap space-x-4">
-              <li>Features</li>
+        {/* Navigation section */}
+        <nav
+          className={`md:static md:flex md:items-center md:w-full
+            ${
+              openMenu
+                ? "absolute flex flex-col top-0 right-0 z-10 min-h-screen w-3/5 gap-y-[21px] bg-white p-8"
+                : "hidden"
+            }
+        `}
+        >
+          {/* Items */}
+          <section className="grow-0 md:flex-grow">
+            <div className="relative block md:hidden mb-10 left-0 right-0 mr-2">
+              <button className="absolute right-0" onClick={handlingMenu}>
+                <CloseIcon />
+              </button>
+            </div>
 
-              <li>Company</li>
+            <ul className="flex flex-col space-y-4 md:flex-row flex-wrap md:space-y-0 space-x-1 md:space-x-4">
+              <li className="text-medium-gray">Features</li>
 
-              <li>Careers</li>
+              <li className="text-medium-gray">Company</li>
 
-              <li>About</li>
+              <li className="text-medium-gray">Careers</li>
+
+              <li className="text-medium-gray">About</li>
             </ul>
           </section>
 
           {/* Login and register */}
-          <section className="hidden md:block">
-            <div className="flex flex-wrap space-x-2">
-              <button className="px-8 py-2 text-medium-gray">Login</button>
+          <section>
+            <div className="flex flex-col space-y-0 md:flex-row flex-wrap md:space-y-2 md:space-x-2">
+              <button className="px-8 py-2 text-medium-gray rounded-xl">
+                Login
+              </button>
               <button className="px-7 py-2 text-medium-gray outline outline-2 outline-medium-gray rounded-xl">
                 Register
               </button>
---
+++ src/components/icons/CloseIcon.jsx
@@ -0,0 +1,18 @@
+const CloseIcon = (props) => {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="#151515"
+      width="26"
+      height="26"
+    >
+      <g fillRule="evenodd">
+        <path d="M2.393.98l22.628 22.628-1.414 1.414L.979 2.395z"></path>
+        <path d="M.98 23.607L23.609.979l1.414 1.414L2.395 25.021z"></path>
+      </g>
+    </svg>
+  );
+};
+
+export default CloseIcon;
